2bcbd66977b98b8a36d28579c0aa962860494dee
Egor <still2095@gmail.com>
2022-05-03 23:22:36 +0300

    v-2

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 require("dotenv").config();
-const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.TOKEN;
+const chatId = process.env.TEST_CHAT_ID;
 
+const TelegramBot = require("node-telegram-bot-api");
+const CronJob = require("cron").CronJob;
 const bot = new TelegramBot(token, { polling: true });
 
 const users = [
   {
     name: "Egor",
-    birthday: "",
+    birthday: "03.07.2002",
+  },
+  {
+    name: "Petya",
+    birthday: "03.05.2022",
   },
 ];
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-  bot.sendMessage(chatId, resp);
+const everyDay = "0 0 */11 * * *";
+
+const everyMinute = "0 */1 * * * *";
+
+const every10Second = "*/10 * * * * *";
+
+const getCurrentDate = () => {
+  const currentDate = new Date().toISOString().slice(0, 10);
+  const [year, month, day] = currentDate.split("-");
+  return `${day}.${month}.${year}`;
+};
+
+const job = new CronJob(everyDay, function () {
+  const currentDate = getCurrentDate();
+  for (user of users) {
+    if (user.birthday === currentDate) {
+      bot.sendMessage(chatId, `happy birthday ${user.name}!!!`);
+    }
+  }
 });
 
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Received your message");
-});
+job.start();
