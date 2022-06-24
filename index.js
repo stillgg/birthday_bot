68a5ca721a0d7aa96755a0991460c5c84a91c8eb
Egor <still2095@gmail.com>
2022-06-24 09:27:23 +0300

    fix bot

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const token = process.env.TOKEN;
-// const chatId = process.env.PRODUCKTV_DEV_CHAT_ID;
-const chatId = process.env.TEST_CHAT_ID;
+const chatId = process.env.PRODUCKTV_DEV_CHAT_ID;
+// const chatId = process.env.TEST_CHAT_ID;
 
 const TelegramBot = require("node-telegram-bot-api");
 const CronJob = require("cron").CronJob;
@@ -15,27 +15,29 @@ const everyDay = "0 12 * * *";
 const everyMinute = "0 */1 * * * *";
 const every5Second = "*/5 * * * * *";
 
-const generateRandomImage = () => `img${randomIntFromInterval(1, 40)}.jpeg`;
+const generateRandomImage = () => `img${randomIntFromInterval(1, 60)}.jpeg`;
 
 const job = new CronJob(everyDay, async function () {
   const currentDate = getCurrentDate();
 
   for (user of users) {
     if (user.birthday === currentDate) {
-      console.log("user", user);
       const img = generateRandomImage();
+      try {
+        await bot.sendPhoto(chatId, `./assets/${img}`);
 
-      await bot.sendPhoto(chatId, `./assets/${img}`);
-
-      await bot.sendMessage(
-        chatId,
-        `
-        <b>Happy birthday ${user.nickname}!!!</b>
-        `,
-        {
-          parse_mode: "HTML",
-        }
-      );
+        await bot.sendMessage(
+          chatId,
+          `
+          <b>Happy birthday ${user.nickname}!!!</b>
+          `,
+          {
+            parse_mode: "HTML",
+          }
+        );
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 });
